681f515047ff00d69209d5e8acc08cd3303322d0
Jason Tuttle <pzx4@txstate.edu>
2024-12-04 11:10:13 -0600

    created dashboard and route

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   return (
     <div>
       <h1>Welcome Home</h1>
+      <Link to="/login">Login</Link>
+      <Link to="/dashboard">Dashboard</Link>
     </div>
   );
 }
--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from '../pages/LoginPage';
 import HomePage from '../pages/HomePage';
+import Dashboard from '../pages/Dashboard';
 
 const AppRoutes: React.FC = () => {
    return (
       <Routes>
          <Route path="/" element={<HomePage />} />
          <Route path="/login" element={<LoginPage />} />
+         <Route path ="/dashboard" element={<Dashboard />} />
       </Routes>
    )
 }
